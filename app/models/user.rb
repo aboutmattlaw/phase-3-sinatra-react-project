a20db5e1480df681c80e23b965bf1b4adc857bde
Matt <aboutmattlaw@gmail.com>
2021-10-25 20:41:28 -0400

    added friendship...can't imaging it's right

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
     has_many :favorites
     has_many :coins, through: :favorites
-        
+    has_many :friendees,  foreign_key: :friendee_id, class_name: 'Friendship'
+    has_many :frienders, through: :friendees     
 end
+
+
 
 
