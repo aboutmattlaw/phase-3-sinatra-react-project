e72fdb8fd0898078f034f4f447458a039086fe61
Matt <aboutmattlaw@gmail.com>
2021-10-26 18:34:59 -0400

    some additional endpoints before dinner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,44 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-
-
   get '/' do
     "Hello World"
   end
+
+# COINS
 
   get '/coins' do
     coins = Coin.all
     coins.to_json
   end
 
+
+  get '/coins/:id' do
+    coins = Coin.find(params[:id])
+    coins.to_json
+  end
+
+# FAVES
+  
   get '/favorites' do
     favorites = Favorite.all
     favorites.to_json
   end
+
+  get '/favorites/:id' do
+    favorites = Favorite.find(params[:id])
+    favorites.to_json
+  end
+
+
+  post '/favorites' do
+    favorites = Favorites.create(user_id: params[:user_id], coin_id: params[:coin_id])
+    favorites.to_json
+  end
+
+
+  # USERS 
+
 
   get '/users' do
     users = User.all
@@ -24,6 +47,12 @@ class ApplicationController < Sinatra::Base
   end
 
 
+  get '/users/:id' do
+    users = User.find(params[:id])
+    users.to_json
+  end
+
+  
   post '/users' do
     user = User.create(username: params[:username], email: params[:email], password: params[:password])
     user.to_json
@@ -31,14 +60,9 @@ class ApplicationController < Sinatra::Base
 
 
 
-  # create_table "users", force: :cascade do |t|
-  #   t.string "username"
-  #   t.string "email"
-  #   t.string "password_digest"
-
-  get '/signup' do
-    "Signup Hello World"
-  end
+  # get '/signup' do
+  #   "Signup Hello World"
+  # end
 
 end
 
