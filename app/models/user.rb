ce04e5eae55cfe431b178dbb62870bf1eb3234c1
Nick <nick.c.ruzicka@gmail.com>
2021-10-28 13:09:45 -0400

    updated-notffriendlist

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,22 @@ class User < ActiveRecord::Base
     has_many :coins, through: :favorites
     has_many :followed_users, class_name: 'Friendship', foreign_key: :friender_id
     has_many :friendees, through: :followed_users
-
     has_many :people_who_follow_me, class_name: 'Friendship', foreign_key: :friendee_id
     has_many :frienders, through: :people_who_follow_me
-
-
     has_secure_password
     validates :username, :email, uniqueness: { case_sensitive: false }
         
+def not_my_friend
+    # one array of all users
+    all_users = User.all
+    # one array of just my friends 
+    all_of_my_freinds = self.friendees
+   
+    # compare arrays
+    not_friends = all_users - all_of_my_freinds
+
+    not_friends.select {|user| user != self}
+end 
 
 
 end
