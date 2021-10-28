da061b8a2a6041cf19768f3c233c7471f058065e
Matt <aboutmattlaw@gmail.com>
2021-10-28 18:01:20 -0400

    dinner push

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ActiveRecord::Base
     belongs_to :friender, class_name: 'User'
     belongs_to :friendee, class_name: 'User'  
+    validates_uniqueness_of   :friender_id, scope: :friendee_id
 end
