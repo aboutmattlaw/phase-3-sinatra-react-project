15465b0757850ed0aa3bd16c110ad8e7cfbdb5e9
Matt <aboutmattlaw@gmail.com>
2021-10-27 21:15:29 -0400

    prevents duplicate faves

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ActiveRecord::Base
     belongs_to :coin
     belongs_to :user
+    validates_uniqueness_of   :coin_id, scope: :user_id
 
     def find_my_faves 
         Favorite.select {|faves|faves.favorites == user.id} 
